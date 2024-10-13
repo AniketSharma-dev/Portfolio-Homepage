677aea79e5475e8ada6c33c16fd4761e40b2e31f
AniketSharma-dev <aniketsharma.xo@gmail.com>
2024-10-13 19:27:02 +0530

    Added File

--- src/app.js
+++ src/app.js
@@ -1,23 +1,42 @@
-var toggleBtn = document.querySelector(".toggle");
-var moon = document.querySelector(".moon");
-var sun = document.querySelector(".sun");
-var bodyy = document.querySelector("body")
+function theme() {
+  var toggleBtn = document.querySelector(".toggle");
+  var moon = document.querySelector(".moon");
+  var sun = document.querySelector(".sun");
+  var maindiv = document.querySelector("#main");
+  var theme = document.querySelectorAll(".theme");
 
-var flag = 0;
-toggleBtn.addEventListener("click", function () {
-  if (flag == 0) {
-    moon.classList.toggle("hidden");
-    sun.classList.toggle("hidden");
-    bodyy.classList.add("light")
-    bodyy.classList.remove("dark")
-    
-    flag = 1;
-  } else {
-    moon.classList.toggle("hidden");
-    sun.classList.toggle("hidden");
-    bodyy.classList.add("dark")
-    bodyy.classList.remove("light")
+  var flag = 0;
+  toggleBtn.addEventListener("click", function () {
+    if (flag == 0) {
+      moon.classList.toggle("hidden");
+      sun.classList.toggle("hidden");
+      maindiv.classList.add("bg-white");
+      theme.forEach((element) => {
+        element.classList.remove("text-white");
+        element.classList.add("text-black");
+      });
 
-    flag = 0;
-  }
+      flag = 1;
+    } else {
+      moon.classList.toggle("hidden");
+      sun.classList.toggle("hidden");
+      maindiv.classList.remove("bg-white");
+      theme.forEach((element) => {
+        element.classList.remove("text-black");
+        element.classList.add("text-white");
+      });
+
+      flag = 0;
+    }
+  });
+}
+
+var ham = document.querySelector(".hamburger");
+var list = document.querySelector("ul");
+
+ham.addEventListener("click", function () {
+  list.classList.toggle("block");
+  list.classList.toggle("hidden");
 });
+
+theme();
